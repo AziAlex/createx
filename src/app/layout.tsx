12ce7aad7de57ef35c7a1d11e5bc00a41085b27c
Alex <102092661+AziAlex@users.noreply.github.com>
2023-09-03 12:16:53 +0300

    Initial commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
-import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+
+import "./style/global.scss"
 
 const inter = Inter({ subsets: ['latin'] })
 
