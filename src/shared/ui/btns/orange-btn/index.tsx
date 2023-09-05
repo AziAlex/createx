f68e3a205011b17b3f393b7fb0a34dd9a1fdad02
Alex <102092661+AziAlex@users.noreply.github.com>
2023-09-05 12:10:59 +0300

    add shared btn

--- src/shared/ui/btns/orange-btn/index.tsx
+++ src/shared/ui/btns/orange-btn/index.tsx
@@ -2,8 +2,24 @@ import React, {FC} from 'react';
 
 import cl from "./style.module.scss"
 
-const OrangeBtn: FC<{ title: string }> = ({title}) => (
-  <button className={cl.button}>{title}</button>
+interface IProps {
+  title: string
+  type: "large" | "regular" | "small"
+  solid?: boolean
+  fill?: boolean
+}
+
+const OrangeBtn: FC<IProps> = ({title, fill, solid, type}) => (
+  <button
+    className={[
+      cl.button,
+      solid ? cl.solid : null,
+      fill ? fill : null,
+      cl[type]
+    ].join(" ")}
+  >
+    {title}
+  </button>
 )
 
 export default OrangeBtn;
