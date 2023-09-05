b31e5c11f2f3c088467691e44f8521611d17f041
Alex <102092661+AziAlex@users.noreply.github.com>
2023-09-05 11:13:15 +0300

    add shared wrap

--- src/page/main-page/index.tsx
+++ src/page/main-page/index.tsx
@@ -1,15 +1,12 @@
 import Header from '@/widgets/header'
 import React from 'react'
-import Slider from "react-slick";
 import MainSlider from "@/feature/main-slider";
 
-const ManiPage = () => {
-  return (
-    <>
-      <Header />
-      <MainSlider/>
-    </>
-  )
-}
+const ManiPage = () => (
+  <>
+    <Header/>
+    <MainSlider/>
+  </>
+)
 
 export default ManiPage
