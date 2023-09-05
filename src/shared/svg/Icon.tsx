75238405c4c44df5329f555609f4806f8967d065
PavelM <myagkiypavel@gmail.com>
2023-09-05 18:58:50 +0300

    feature; add icons for OurValues

--- src/shared/svg/Icon.tsx
+++ src/shared/svg/Icon.tsx
@@ -1,18 +1,21 @@
-export * from "./ui/additional/IconLogo"
-export * from "./ui/additional/IconPhone"
+export * from './ui/additional/IconHand'
+export * from './ui/additional/IconLike'
+export * from './ui/additional/IconLogo'
+export * from './ui/additional/IconPhone'
+export * from './ui/additional/IconSlippers'
 
-export * from "./ui/arrows/IconArrowDown"
-export * from "./ui/arrows/IconArrowLeft"
-export * from "./ui/arrows/IconArrowRight"
-export * from "./ui/arrows/IconArrowTop"
+export * from './ui/arrows/IconArrowDown'
+export * from './ui/arrows/IconArrowLeft'
+export * from './ui/arrows/IconArrowRight'
+export * from './ui/arrows/IconArrowTop'
 
-export * from "./ui/arrows-other/IconArrowConvert"
+export * from './ui/arrows-other/IconArrowConvert'
 
-export * from "./ui/basic/IconBurgerMenu"
+export * from './ui/basic/IconBurgerMenu'
 
-export * from "./ui/chevrons/IconChevronsDown"
-export * from "./ui/chevrons/IconChevronsTop"
+export * from './ui/chevrons/IconChevronsDown'
+export * from './ui/chevrons/IconChevronsTop'
 
-export * from "./ui/communications/IconChat"
+export * from './ui/communications/IconChat'
 
-export * from "./ui/plus-minus-cros/IconCross"
+export * from './ui/plus-minus-cros/IconCross'
