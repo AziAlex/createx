d3b9a1c8dc13f8c47eaf32e9099f21b39038dbe3
Alex <102092661+AziAlex@users.noreply.github.com>
2023-09-08 23:49:27 +0300

    edit recent-news

--- src/page/main-page/ui/recent-news/index.tsx
+++ src/page/main-page/ui/recent-news/index.tsx
@@ -1,28 +1,25 @@
 import Wrap from '@/shared/ui/wrap'
 import Button from '@/shared/ui/btns/btn'
 import NewsCard from '@/entities/news-card'
-
-import styles from './style.module.scss'
 import { newsDbData } from '@/shared/data/newsDb'
 
-const RecentNews = async () => {
+import styles from './style.module.scss'
 
-  return (
-    <Wrap>
-      <div className={styles.recentNews}>
-        <h2>Recent news</h2>
-        <div className={styles.newsBlock}>
-          <NewsCard card={newsDbData[0]} />
-          <NewsCard card={newsDbData[1]} minCard />
-          <NewsCard card={newsDbData[2]} minCard />
-        </div>
-        <div className={styles.footer}>
-          <h3>Explore all our news posts</h3>
-          <Button title='View all news' size='large' solid />
-        </div>
+const RecentNews = () => (
+  <Wrap>
+    <div className={styles.recentNews}>
+      <h2>Recent news</h2>
+      <div className={styles.newsBlock}>
+        <NewsCard card={newsDbData[0]} />
+        <NewsCard card={newsDbData[1]} minCard />
+        <NewsCard card={newsDbData[2]} minCard />
       </div>
-    </Wrap>
-  )
-}
+      <div className={styles.footer}>
+        <h3>Explore all our news posts</h3>
+        <Button title='View all news' size='large' solid />
+      </div>
+    </div>
+  </Wrap>
+)
 
 export default RecentNews
