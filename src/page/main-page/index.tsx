52ac10c30c2194de493091ca89eee58105e74603
PavelM <myagkiypavel@gmail.com>
2023-09-08 18:31:54 +0300

    refactor: path in MainPage

--- src/page/main-page/index.tsx
+++ src/page/main-page/index.tsx
@@ -1,9 +1,9 @@
-import Header from '@/widgets/header'
-import MainSlider from '../../feature/sliders/main-slider'
-import OurValues from './ui/our-values'
-import VideoBlock from '@/page/main-page/ui/video-block'
+import AskUs from '@/page/main-page/ui/ask-us'
 import ClientPartners from '@/page/main-page/ui/client-wrap'
-import AskUs from './ui/ask-us'
+import Header from '@/widgets/header'
+import MainSlider from '@/feature/sliders/main-slider'
+import OurValues from '@/page/main-page/ui/our-values'
+import VideoBlock from '@/page/main-page/ui/video-block'
 
 
 const ManiPage = () => (
