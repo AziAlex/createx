b7fde09b7da25ae964f5e70fe89629fb55ae6ac0
Alex <102092661+AziAlex@users.noreply.github.com>
2023-09-08 15:52:42 +0300

    edit on recent-block

--- src/page/main-page/ui/recent-news/index.tsx
+++ src/page/main-page/ui/recent-news/index.tsx
@@ -2,15 +2,17 @@ import React from 'react'
 import Wrap from '@/shared/ui/wrap'
 import Button from '@/shared/ui/btns/btn'
 
-import styles from "./style.module.scss"
+import styles from './style.module.scss'
 
 const RecentNews = () => {
   return (
     <Wrap>
-      <h2>Recent news</h2>
-      <div className={styles.footer}>
-        <h3>Explore all our news posts</h3>
-        <Button title="View all news" size="large" solid/>
+      <div className={styles.recentNews}>
+        <h2>Recent news</h2>
+        <div className={styles.footer}>
+          <h3>Explore all our news posts</h3>
+          <Button title='View all news' size='large' solid />
+        </div>
       </div>
     </Wrap>
   )
