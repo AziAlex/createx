7f0ae93d2e8f265fc0627aa5bfc93094f4a06027
PavelM <myagkiypavel@gmail.com>
2023-09-10 19:52:55 +0300

    refactor

--- src/page/main-page/ui/our-services/index.tsx
+++ src/page/main-page/ui/our-services/index.tsx
@@ -8,32 +8,29 @@ import { cards } from './lib/constants'
 
 import styles from './style.module.scss'
 
-const OurServices = () => {
-  return (
-    <div className={styles.wrap}>
-      <Image src={'/main-page/bg-image.png'} alt="bg" fill />
-      <Wrap>
-        <div className={styles.our_services}>
-          <h2 className={styles.title}>Our services</h2>
-          <p className={styles.text}>
-            Createx Construction Bureau is a construction giant with a full
-            range of construction services.
-          </p>
+const OurServices = () => (
+  <div className={styles.wrap}>
+    <Image src={'/main-page/bg-image.png'} alt="bg" fill />
+    <Wrap>
+      <div className={styles.our_services}>
+        <h2 className={styles.title}>Our services</h2>
+        <p className={styles.text}>
+          Createx Construction Bureau is a construction giant with a full range
+          of construction services.
+        </p>
 
-          <div className={styles.cards}>
-            {cards.map((card, idx) => (
-              <Card key={idx} card={card} />
-            ))}
-          </div>
-
-        
-          <FooterBlock title="Learn more about our services">
-            <Button title="View services" size="large" solid />
-          </FooterBlock>
+        <div className={styles.cards}>
+          {cards.map((card, idx) => (
+            <Card key={idx} card={card} />
+          ))}
         </div>
-      </Wrap>
-    </div>
-  )
-}
+
+        <FooterBlock title="Learn more about our services">
+          <Button title="View services" size="large" solid />
+        </FooterBlock>
+      </div>
+    </Wrap>
+  </div>
+)
 
 export default OurServices
