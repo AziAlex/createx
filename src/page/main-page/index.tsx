344d179cf25bd3a20ace0edbc3d900da2ed6f753
PavelM <myagkiypavel@gmail.com>
2023-09-10 19:12:33 +0300

    chore: Main

--- src/page/main-page/index.tsx
+++ src/page/main-page/index.tsx
@@ -5,7 +5,7 @@ import MainSlider from '@/feature/sliders/main-slider'
 import OurValues from '@/page/main-page/ui/our-values'
 import VideoBlock from '@/page/main-page/ui/video-block'
 import RecentNews from '@/page/main-page/ui/recent-news'
-
+import OurServices from './ui/our-services'
 
 const ManiPage = () => (
   <>
@@ -14,6 +14,7 @@ const ManiPage = () => (
     <VideoBlock />
     <OurValues />
     <AskUs />
+    <OurServices />
     <ClientPartners />
     <RecentNews />
   </>
