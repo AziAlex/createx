6b49dc36bd8e07c5ec693f50848d3b991ee5492d
Alex <102092661+AziAlex@users.noreply.github.com>
2023-09-12 15:15:55 +0300

    fix full request

--- src/feature/page-routing/index.tsx
+++ src/feature/page-routing/index.tsx
@@ -1,23 +1,21 @@
 'use client'
 
-import React from 'react'
 import { usePathname } from 'next/navigation'
+import Link from 'next/link'
 
 import styles from './style.module.scss'
-import Link from 'next/link'
 
 const PageRouter = () => {
   const patchName = usePathname()
 
-  const patchList = ['Homepage', ...patchName.split('/').filter(item => item !== '')]
+  const patchList = ['homepage', ...patchName.split('/').filter(item => item !== '')]
 
-  console.log(patchList)
   return (
     <ul className={styles.routList}>
       {patchList.map((item, idx) => (
         <li key={idx}>
           <Link href={item === 'Homepage' ? '/' : `/${item}`}>
-            {idx === 0 ? '' : <span>/ </span>}{item}
+            {idx === 0 ? '' : <span>/</span>}{item}
           </Link>
         </li>
       ))}
