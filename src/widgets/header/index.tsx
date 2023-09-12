201a05b16b302b5ca7cb341a54b2a6db632d62ab
Alex <102092661+AziAlex@users.noreply.github.com>
2023-09-13 00:34:04 +0300

    fix: pixel perfect on header

--- src/widgets/header/index.tsx
+++ src/widgets/header/index.tsx
@@ -1,55 +1,65 @@
-import {links} from '@/widgets/header/lib/constants'
+'use client'
+
+import { links } from '@/widgets/header/lib/constants'
 import Wrap from '@/shared/ui/wrap'
 import Menu from './ui/menu'
 import Icon from '@/shared/svg'
 
-import cl from './style.module.scss'
+import styles from './style.module.scss'
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 
-const Header = () => (
-  <Wrap>
-    <div className={cl.header}>
-      <div className={cl.navigation}>
-        <div>
-          <Link href="/">
-            <Icon name="logo" className={cl.logo}/>
-          </Link>
+const Header = () => {
+  const patchName = usePathname()
+
+  return (
+    <Wrap>
+      <div className={styles.header}>
+        <div className={styles.navigation}>
+          <div>
+            <Link href='/'>
+              <Icon name='logo' className={styles.logo} />
+            </Link>
+          </div>
+
+          <nav>
+            <ul>
+              {links.map((link) => (
+                <li key={link.name}>
+                  <a
+                    href={link.href}
+                    className={patchName === link.href ? styles.active : ''}
+                  >{link.name}</a>
+                </li>
+              ))}
+            </ul>
+          </nav>
         </div>
 
-        <nav>
-          <ul>
-            {links.map((link) => (
-              <li key={link.name}>
-                <a href={link.href}>{link.name}</a>
-              </li>
-            ))}
-          </ul>
-        </nav>
-      </div>
+        <div className={styles.contacts}>
+          <div className={styles.contact}>
+            <Icon name='phone' className={styles.svg} />
+            <div>
+              <h4 className={styles.title}>Call us</h4>
+              <p className={styles.text}>(405) 555-0128</p>
+            </div>
+          </div>
 
-      <div className={cl.contacts}>
-        <div className={cl.contact}>
-          <Icon name="phone" className={cl.svg}/>
-          <div>
-            <h4 className={cl.title}>Call us</h4>
-            <p className={cl.text}>(405) 555-0128</p>
+          <div className={styles.contact}>
+            <Icon name='chat' className={styles.svg} />
+            <div>
+              <h4 className={styles.title}>Talk to us</h4>
+              <p className={styles.text}>hello@createx.com</p>
+            </div>
           </div>
         </div>
 
-        <div className={cl.contact}>
-          <Icon name="chat" className={cl.svg}/>
-          <div>
-            <h4 className={cl.title}>Talk to us</h4>
-            <p className={cl.text}>hello@createx.com</p>
-          </div>
+        <div className={styles.menu}>
+          <Menu />
         </div>
       </div>
-
-      <div className={cl.menu}>
-        <Menu/>
-      </div>
-    </div>
-  </Wrap>
-)
+    </Wrap>
+  )
+}
 
 export default Header
