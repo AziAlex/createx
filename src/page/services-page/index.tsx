659c90dde46494b3d0bd94f47bb3ed339521f9da
Alex <102092661+AziAlex@users.noreply.github.com>
2023-09-12 23:37:14 +0300

    feat: add service-page

--- src/page/services-page/index.tsx
+++ src/page/services-page/index.tsx
@@ -2,10 +2,12 @@ import React from 'react'
 import FooterForm from '@/widgets/footer-form'
 import Footer from '@/widgets/footer'
 import HeaderServices from '@/page/services-page/ui/header'
+import MainServices from '@/page/services-page/main'
 
 const ServicePage = () => (
   <>
     <HeaderServices />
+    <MainServices />
     <FooterForm />
     <Footer />
   </>
