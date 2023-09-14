eb4c196e81946e85dd7deec03ff0b252626887c9
PavelM <myagkiypavel@gmail.com>
2023-09-14 16:27:53 +0300

    refactor

--- src/page/services/interior-design/ui/pricing/index.tsx
+++ src/page/services/interior-design/ui/pricing/index.tsx
@@ -1,28 +1,29 @@
 'use client'
-import React, { FC } from 'react'
+
+import { FC } from 'react'
 import Image from 'next/image'
 
 import Wrap from '@/shared/ui/wrap'
 import PartnersSlider from '@/feature/sliders/partners-slider'
 import PricingTable from '@/page/services/interior-design/ui/pricing/ui/table'
 
-import styles from '@/page/services/interior-design/ui/pricing/style.module.scss'
+import styles from './style.module.scss'
 
-const Pricing: FC = () => {
-  return (
-    <div className={styles.wrap}>
-      <Image src={'/pages/services/interior-design/table-section-bg.png'} alt='bg' width={560} height={670} />
-      <Wrap>
-        <h2>Pricing</h2>
-        <p>We offer you three categories of construction.</p>
-        <PricingTable />
-        <PartnersSlider
-          rows={1}
-          title='Supported by 12+ partners'
-        />
-      </Wrap>
-    </div>
-  )
-}
+const Pricing: FC = () => (
+  <div className={styles.wrap}>
+    <Image
+      src={'/pages/services/interior-design/table-section-bg.png'}
+      alt="bg"
+      width={560}
+      height={670}
+    />
+    <Wrap>
+      <h2>Pricing</h2>
+      <p>We offer you three categories of construction.</p>
+      <PricingTable />
+      <PartnersSlider rows={1} title="Supported by 12+ partners" />
+    </Wrap>
+  </div>
+)
 
 export default Pricing
--- src/page/services/interior-design/ui/pricing/ui/table/index.tsx
+++ src/page/services/interior-design/ui/pricing/ui/table/index.tsx
@@ -1,52 +1,49 @@
 'use client'
-import React, { FC } from 'react'
 
+import { tableData } from '@/page/services/interior-design/ui/pricing/lib/constants'
 import Button from '@/shared/ui/btns/btn'
 import Icon, { IconName } from '@/shared/svg'
-import { tableData } from '@/page/services/interior-design/lib/tableData'
 
 import styles from './style.module.scss'
 
-const PricingTable: FC = () => (
+const PricingTable = () => (
   <div className={styles.tableWrap}>
     <table>
       <thead>
-      <tr>
-        {tableData.tableHeader.map((item, index) => (
-          <th key={index}>
-            <p>{item.title}</p>
-            {item.subtitle && (
-              <p>{item.subtitle}</p>
-            )}
-          </th>
-        ))}
-      </tr>
+        <tr>
+          {tableData.tableHeader.map((item, index) => (
+            <th key={index}>
+              <p>{item.title}</p>
+              {item.subtitle && <p>{item.subtitle}</p>}
+            </th>
+          ))}
+        </tr>
       </thead>
 
       <tbody>
-      {tableData.tableBody.map((item, index) => (
-        <tr key={index}>
-          {item.map((item, index) => (
+        {tableData.tableBody.map((item, index) => (
+          <tr key={index}>
+            {item.map((item, index) => (
+              <td key={index}>
+                {item.body}
+                {item.type === 'icon' && item.iconName && (
+                  <Icon name={item.iconName} className={styles.checkmark} />
+                )}
+              </td>
+            ))}
+          </tr>
+        ))}
+
+        <tr>
+          {tableData.tableFooter.map((item, index) => (
             <td key={index}>
-              {item.body && item.body}
-              {item.type === 'icon' && (
-                <Icon name={item.iconName as IconName} className={styles.checkmark} />
+              {item.body}
+              {item.type === 'button' && item.props && (
+                <Button {...item.props} />
               )}
             </td>
           ))}
         </tr>
-      ))}
-
-      <tr>
-        {tableData.tableFooter.map((item, index) => (
-          <td key={index}>
-            {item.body && item.body}
-            {item.type === 'button' && item.props && (
-              <Button {...item.props} />
-            )}
-          </td>
-        ))}
-      </tr>
       </tbody>
     </table>
   </div>
