b8fb42cc4adbb02466f8e19a6e1df8629147b1e0
PavelM <myagkiypavel@gmail.com>
2023-09-15 19:03:57 +0300

    feat: add RelatedProjects

---
+++ src/page/services/interior-design/ui/related-projects/lib/constants.tsx
@@ -0,0 +1,40 @@
+import { TCard } from '@/shared/types/projects'
+
+export const cards: TCard[] = [
+  {
+    img: '/show-cards/KidsBedroom.jpg',
+    alt: 'Finger Building',
+    title: 'Kids Bedroom With Decorations',
+    description: 'Apartments & flats',
+  },
+  {
+    img: '/show-cards/ModernBedroom.jpg',
+    alt: 'Cubes Building',
+    title: 'Modern Double Bedroom',
+    description: 'Apartments & flats',
+  },
+  {
+    img: '/show-cards/ScandinavianBedroom.jpg',
+    alt: 'Pencil Building',
+    title: 'Scandinavian Style Interior',
+    description: 'Private houses',
+  },
+  {
+    img: '/show-cards/KidsBedroom.jpg',
+    alt: 'Finger Building',
+    title: 'Kids Bedroom With Decorations',
+    description: 'Apartments & flats',
+  },
+  {
+    img: '/show-cards/ModernBedroom.jpg',
+    alt: 'Cubes Building',
+    title: 'Modern Double Bedroom',
+    description: 'Apartments & flats',
+  },
+  {
+    img: '/show-cards/ScandinavianBedroom.jpg',
+    alt: 'Pencil Building',
+    title: 'Scandinavian Style Interior',
+    description: 'Private houses',
+  },
+]
