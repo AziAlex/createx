8b5770c2c4d6fea5dca1971465a928e3a4876564
Alex <102092661+AziAlex@users.noreply.github.com>
2023-09-19 18:13:33 +0300

    refactor: layout

--- src/page/services/interior-design/index.tsx
+++ src/page/services/interior-design/index.tsx
@@ -1,19 +1,19 @@
-import React from 'react'
-import FooterForm from '@/widgets/footer-form'
-import Footer from '@/widgets/footer'
-import InteriorDesignHeader from '@/page/services/interior-design/ui/header'
 import Pricing from '@/page/services/interior-design/ui/pricing'
 import OurBenefits from './ui/our-benefits'
 import RelatedProjects from './ui/related-projects'
+import HeaderInfo from '@/widgets/header-info'
 
 const InteriorDesignPage = () => (
   <>
-    <InteriorDesignHeader />
+    <HeaderInfo
+      img='/pages/services/interior-design/header.png'
+      title='INTERIOR DESIGN'
+      description='Dui augue nec mi mi. Ut ac lectus donec fames pellentesque. Laoreet aenean vulputate elementum blandit amet.'
+      imgWidth={944}
+    />
     <OurBenefits />
     <RelatedProjects />
     <Pricing />
-    <FooterForm />
-    <Footer />
   </>
 )
 
