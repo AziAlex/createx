5ea5034143be90bd31190eb7745ab557f891fcb6
PavelM <myagkiypavel@gmail.com>
2023-09-21 12:02:32 +0300

    test: group routing

---
+++ src/app/(marketing)/layout.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const MarketingLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div>
+      MarketingLayout
+      {children}
+    </div>
+  )
+}
+
+export default MarketingLayout
---
+++ src/app/(shop)/layout.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function PostLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <div>
+      Shop Layout
+      {children}
+    </div>
+  )
+}
+
+export default PostLayout
